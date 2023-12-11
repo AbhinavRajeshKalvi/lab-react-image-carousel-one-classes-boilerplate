f6e336210d8c9c00a4022ce370f6822f911fb695
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2023-12-11 17:09:43 +0530

    image-carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,10 +4,63 @@ import { images } from "../data/CarouselData";
 // you can explore more - and check as how to use materiul ui
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { CurrencyBitcoin } from "@mui/icons-material";
 
 // implement the class below
 class Carousel extends Component {
-  
+    constructor(){
+        super();
+
+
+        //setting an initial state
+        this.state={
+            currIndex : 0
+        }
+    }
+    
+    increaseCount = () =>{
+        if(this.state.currIndex >= images.length - 1){
+            this.setState({
+                currIndex : 0
+            })
+        }else{
+            this.setState({
+                currIndex : this.state.currIndex + 1,
+            })
+        }
+}
+
+    decreaseCount = () =>{
+        if(this.state.currIndex <= 0){
+            this.setState({
+                currIndex : 2
+            })
+        }else{
+            this.setState({
+                currIndex : this.state.currIndex + -1,
+            })
+        }
+    }
+
+    componentDidUpdate(){
+        console.log(this.state)
+    }
+
+    render(){
+        return <>
+            <div className="carousel-container flex " >
+                <div className="leftArrow arrowDiv flex" onClick={this.decreaseCount}>
+                    <ArrowBackIosIcon className="x1" />
+                </div>
+                <h2 className="Title flex" >{images[this.state.currIndex].title}</h2>
+                <img src={images[this.state.currIndex].img} alt="" />
+                <h2 className="Caption flex" >{images[this.state.currIndex].subtitle}</h2>
+                <div className="rightArrow arrowDiv flex" onClick={this.increaseCount}>
+                    < ArrowForwardIosIcon/>
+                </div>
+            </div>
+        </>
+    }
 }
 
 export default Carousel;
